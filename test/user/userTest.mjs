498f999c837183670cd7a783ca63fcc2e2590c78
Selase Krakani <skrakani@gmail.com>
2018-10-25 07:31:46 +0000

    Setup linting via eslint
    Setup linting for the project

--- test/user/userTest.mjs
+++ test/user/userTest.mjs
@@ -1,9 +1,8 @@
 import { assert } from 'chai';
-import User from '../../api/controllers/user'
+import User from '../../api/controllers/user';
 
-
-describe('User',()=>{
-  it('User should return hello',() => {
-    assert.equal(User.sayHello(),'hello');
+describe('User', () => {
+  it('User should return hello', () => {
+    assert.equal(User.sayHello(), 'hello');
   });
 });
